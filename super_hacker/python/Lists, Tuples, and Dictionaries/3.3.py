1da2caff108fd6b8971edc6ae8d4527ed3cb63fe
UndifendP <mahmoudhesham385@gmail.com>
2025-06-25 14:49:09 +0300

    Create 3.3.py

---
+++ super_hacker/python/Lists, Tuples, and Dictionaries/3.3.py
@@ -0,0 +1,1 @@
+status_code_dict={'200':'OK','201':'Created','202':'Accepted','204':'No Content','400':'Bad Request','401':'Unauthorized','403':'Forbidden','404':'Not Found','405':'Method Not Allowed','406':'Not Acceptable','500':'Internal Server Error','502':'Bad Gateway','503':'Service Unavailable','504':'Gateway Timeout'}
